8cead3ebfc0911ebabd8f9e05d7bd8cc232c0936
Nari Jeong <nari@Naris-MacBook-Pro.local>
2019-08-20 19:49:03 +0900

    add views and template for post_list and post_detail

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+# Custom manager that returns the published post objects
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().filter(status='published')
+        return super(PublishedManager, self).get_queryset().filter(status="published")
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -20,8 +22,14 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects = models.Manager() # The default manager
+    published = PublishedManager() # Custom manager
+
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.publish.year, self.publish.strftime('%m'), self.publish.strftime('%d'), self.slug])
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+# TODO: delete this
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    # TODO: delete these
+    # url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
+    # url(r'^blog/', include('blog.urls')),
 ]
