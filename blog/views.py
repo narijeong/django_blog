4ec542e93dab94f78e3cf99200098984c5f48c62
Nari Jeong <nari@Naris-MacBook-Pro.local>
2019-09-04 05:36:04 +0900

    add showing similar posts functionality

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic.list import ListView
 from .forms import EmailPostForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 def post_list(request, tag_slug=None):
     object_list = Post.published.all()
@@ -34,7 +35,12 @@ class PostListView(ListView):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     # Retrieve post by id
