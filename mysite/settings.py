e036660969fdc5887eae40c35fc8ed6c82f05a46
Nari Jeong <nari@Naris-MacBook-Pro.local>
2019-09-08 17:55:06 +0900

    add STATIC_TMP variable in settings.py to fix collectstatic issue while pushing

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,12 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+os.makedirs(STATIC_TMP, exist_ok=True)
+
+
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'narivoyles@gmail.com'
